93008a246d9fceff782c3a820447465df6d58e7d
huyong007 <1341696804@qq.com>
2019-04-08 09:17:07 +0800

    canvas clock demo

---
+++ canvas-clock/clock.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90;
+drawClock();
+
+function drawClock() {
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    drawFace(ctx, radius)
+}
+
+function drawFace(ctx, radius) {
+    var grad;
+    ctx.beginPath();
+}
